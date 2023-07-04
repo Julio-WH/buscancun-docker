a55e6e429380ac728cf6ba9544231ca9f3ccc072
Julio-WH <julio@wisphub.net>
2023-07-04 17:59:10 -0500

    Integracion asgi

--- Buscancun/asgi.py
+++ Buscancun/asgi.py
@@ -1,12 +1,14 @@
 import os
 import django
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from .routing import websocket_urlpatterns
+# from channels.auth import AuthMiddlewareStack
 from channels.http import AsgiHandler
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Buscancun.settings')
 django.setup()
+
+from .routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter(
     {
         'http': AsgiHandler(),
--- Buscancun/consumers.py
+++ Buscancun/consumers.py
@@ -21,59 +21,59 @@ from djangochannelsrestframework.mixins import (
 )
 
 
-class MyConsumer(AsyncWebsocketConsumer):
-    
-    async def connect(self):
-        self.room_name = 'sala_unica'
-
-        # Unirse al grupo de la sala
-        await self.channel_layer.group_add(
-            self.room_name,
-            self.channel_name
-        )
-        await self.accept()
-        await self.get_new_data()
-
-
-    async def disconnect(self, close_code):
-        pass
-
-    async def receive(self, text_data):
-        # Recibir mensaje del cliente WebSocket
-        # y enviarlo a todos los clientes en la misma sala
-        await self.channel_layer.group_send(
-            self.room_name,
-            {
-                'type': 'send_message',
-                'message': text_data
-            }
-        )
-
-
-    # Función para obtener nuevos datos en tiempo real
-    async def get_new_data(self):
-        # Obtén los nuevos datos de tu modelo
-        new_data = sync_to_async(get_autobuses)()  # Agrega tu lógica de filtrado si es necesario
-        # new_data = await Autobus.obtener_autobuses_activos()
-        resultado = await new_data
-        print(resultado)
-        # Convierte los nuevos datos en un formato adecuado para el envío
-        formatted_data = []  # Formatea los datos según tus necesidades
-
-        # Envia los nuevos datos a los clientes suscritos
-        # async_to_sync(self.channel_layer.group_send)("sala_unica", {"type": "send_message", "message": formatted_data})
-        await self.channel_layer.group_send(
-            self.room_name,
-            {
-                'type': 'send_message',
-                'message': json.dumps(resultado)
-            }
-        )
-
-    async def send_message(self, event):
-        # Enviar mensaje a todos los clientes en la misma sala
-        message = event['message']
-        await self.send(text_data=message)
+# class MyConsumer(AsyncWebsocketConsumer):
+#
+#     async def connect(self):
+#         self.room_name = 'sala_unica'
+#
+#         # Unirse al grupo de la sala
+#         await self.channel_layer.group_add(
+#             self.room_name,
+#             self.channel_name
+#         )
+#         await self.accept()
+#         await self.get_new_data()
+#
+#
+#     async def disconnect(self, close_code):
+#         pass
+#
+#     async def receive(self, text_data):
+#         # Recibir mensaje del cliente WebSocket
+#         # y enviarlo a todos los clientes en la misma sala
+#         await self.channel_layer.group_send(
+#             self.room_name,
+#             {
+#                 'type': 'send_message',
+#                 'message': text_data
+#             }
+#         )
+#
+#
+#     # Función para obtener nuevos datos en tiempo real
+#     async def get_new_data(self):
+#         # Obtén los nuevos datos de tu modelo
+#         new_data = sync_to_async(get_autobuses)()  # Agrega tu lógica de filtrado si es necesario
+#         # new_data = await Autobus.obtener_autobuses_activos()
+#         resultado = await new_data
+#         print(resultado)
+#         # Convierte los nuevos datos en un formato adecuado para el envío
+#         formatted_data = []  # Formatea los datos según tus necesidades
+#
+#         # Envia los nuevos datos a los clientes suscritos
+#         # async_to_sync(self.channel_layer.group_send)("sala_unica", {"type": "send_message", "message": formatted_data})
+#         await self.channel_layer.group_send(
+#             self.room_name,
+#             {
+#                 'type': 'send_message',
+#                 'message': json.dumps(resultado)
+#             }
+#         )
+#
+#     async def send_message(self, event):
+#         # Enviar mensaje a todos los clientes en la misma sala
+#         message = event['message']
+#         await self.send(text_data=message)
 
 
 class AutobusConsumer(ListModelMixin, RetrieveModelMixin, PatchModelMixin, UpdateModelMixin, CreateModelMixin,
