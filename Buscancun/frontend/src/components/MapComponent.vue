1203c68914b0411baf08568076b68847df5b0b78
Julio PCGamer <julio9988952669@gmail.com>
2024-01-26 22:31:20 -0500

    Integracion de firebases

--- Buscancun/frontend/src/components/MapComponent.vue
+++ Buscancun/frontend/src/components/MapComponent.vue
@@ -8,6 +8,8 @@
 
 <script>
 import L from "leaflet";
+import { initializeApp } from "firebase/app";
+import { getDatabase, ref as firbaseRef, onValue } from "firebase/database";
 export default {
   name: "MapComponent",
   mounted() {
@@ -15,21 +17,59 @@ export default {
   },
   methods: {
     initMap() {
-      const map = L.map("map").setView(
-        [21.162310281950226, -86.8530133090876],
-        14
-      );
+      let data = { lat: 21.151115, lng: -86.904942 };
+      const firebaseConfig = JSON.parse(process.env.VUE_APP_FIRE_CONFIG);
+
+      var myIcon = L.icon({
+        iconUrl: require("@/assets/bus-stop.png"),
+        iconSize: [40, 40],
+        iconAnchor: [19, 35],
+        popupAnchor: [0, -20],
+      });
+      const coordenadas = [data.lat, data.lng];
+      const map = L.map("map").setView(coordenadas, 14);
       const tileLayer = L.tileLayer(
-        "https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=51a31031f86a4699994325648e4af7f9",
+        "https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey=" +
+          process.env.VUE_APP_API_MAPS,
         {
           maxZoom: 19,
           minZoom: 14,
         }
       );
+      let market1 = L.marker(coordenadas, { icon: myIcon })
+        .addTo(map)
+        .bindPopup("<p>Ruta:44</p><p>Placa:xl33aaaa</p>");
       tileLayer.addTo(map);
+
+      var latlngs = [
+        coordenadas,
+        [21.151115, -86.904942],
+        [21.151237, -86.904776],
+        [21.151085, -86.904705],
+        [21.150891, -86.904632],
+        [21.150832, -86.90463],
+        [21.150707, -86.90453],
+        [21.150636, -86.90451],
+        [21.150584, -86.904436],
+      ];
+
+      L.polyline(latlngs, { color: "red" }).addTo(map);
+      // Initialize Firebase
+      const firebaseApp = initializeApp(firebaseConfig);
+      const database = getDatabase(firebaseApp);
+      const dbRef = firbaseRef(database, `/autobuses`);
+      onValue(dbRef, (snapshot) => {
+        data = snapshot.val()[1];
+        var latlng = L.latLng(data.lat, data.lng);
+        market1.setLatLng(latlng);
+      });
     },
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.leaflet-div-icon {
+  background-color: red;
+}
+</style>
